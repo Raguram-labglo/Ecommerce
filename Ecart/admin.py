3d0debd52a5dcce2fc4caa8ad0ead0e531c2b9cc
Raguram-labglo <raguram.labglo@gmail.com>
2022-10-25 17:56:39 +0530

    Order m2m process

--- Ecart/admin.py
+++ Ecart/admin.py
@@ -3,5 +3,11 @@ from Ecart.models import *
 
 
 admin.site.register(Products_details)
-admin.site.register(Carts)
-admin.site.register(Order)
+
+class Cart_list(admin.ModelAdmin):
+    list_display = ('id','user', 'product', 'price', 'quantity')
+admin.site.register(Carts, Cart_list)
+
+class Order_list(admin.ModelAdmin):
+    list_display = ('id','user', 'order_status')
+admin.site.register(Order, Order_list)
